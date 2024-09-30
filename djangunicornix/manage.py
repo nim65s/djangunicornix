4d91a5b88da1b2d858c8d6bf7f44201cde0217cf
Guilhem Saurel <guilhem.saurel@laas.fr>
2024-09-30 14:18:05 +0200

    django-admin startproject djangunicornix

---
+++ djangunicornix/manage.py
@@ -0,0 +1,22 @@
+#!/nix/store/h3i0acpmr8mrjx07519xxmidv8mpax4y-python3-3.12.5/bin/python
+"""Django's command-line utility for administrative tasks."""
+import os
+import sys
+
+
+def main():
+    """Run administrative tasks."""
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangunicornix.settings")
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+    execute_from_command_line(sys.argv)
+
+
+if __name__ == "__main__":
+    main()
