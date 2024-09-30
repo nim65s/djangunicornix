6bbf1ca7b2f9f1ebf55ce3fd1fb02eb2c597838b
Guilhem Saurel <guilhem.saurel@laas.fr>
2024-09-30 14:27:45 +0200

    nix packaging of django app

--- package.nix
+++ package.nix
@@ -2,17 +2,22 @@
   lib,
   python3Packages,
 }:
-python3Packages.buildPythonPackage {
+python3Packages.buildPythonApplication {
   pname = "gunicornix";
   version = "0.0.1";
+  pyproject = true;
 
   src = lib.fileset.toSource {
     root = ./.;
     fileset = lib.fileset.unions [
-      #./djangunicornix
+      ./djangunicornix
+      ./pyproject.toml
     ];
   };
 
+  build-system = with python3Packages; [
+    setuptools
+  ];
   dependencies = with python3Packages; [
     django
   ];
