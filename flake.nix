06d4af49b9a194b25a346c1cc1e899330ca16b7f
Guilhem Saurel <guilhem.saurel@laas.fr>
2024-09-30 14:23:08 +0200

    treefmt

--- flake.nix
+++ flake.nix
@@ -2,25 +2,50 @@
   description = "Django app packaged with gunicorn on NixOS";
 
   inputs = {
-    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
     inputs:
     inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [ inputs.treefmt-nix.flakeModule ];
       systems = [ "x86_64-linux" ];
       perSystem =
-        { pkgs, self', ... }:
+        { config, pkgs, self', ... }:
         {
           apps.default = {
             type = "app";
             program = pkgs.python3.withPackages (_: [ self'.packages.default ]);
           };
-          devShells.default = pkgs.mkShell { inputsFrom = [ self'.packages.default ]; };
+          devShells.default = pkgs.mkShell {
+            nativeBuildInputs = [ config.treefmt.build.wrapper ];
+            inputsFrom = [ self'.packages.default ];
+          };
           packages = {
             default = self'.packages.djangunicornix;
             djangunicornix = pkgs.python3Packages.callPackage ./package.nix { };
+          };
+          treefmt = {
+            projectRootFile = "flake.nix";
+            programs = {
+              deadnix.enable = true;
+              mdformat.enable = true;
+              nixfmt-rfc-style.enable = true;
+              ruff-check.enable = true;
+              ruff-format.enable = true;
+              toml-sort = {
+                enable = true;
+                all = true;
+              };
+            };
           };
         };
     };
